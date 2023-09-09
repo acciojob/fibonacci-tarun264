48927a85f92b5521abbfe0727dc771d3e01afc9e
Tarun Rana <66361151+tarun264@users.noreply.github.com>
2023-09-09 22:57:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function fibonacci(num) {
-// your code here
+ if(num==1){
+        return 0;
+    }
+    if(num==2){
+        return 1;
+    }
+    return fibonacci(num-1)+fibonacci(num-2);
 }
+console.log("fibonacci(1)",fibonacci(1));
+console.log("fibonacci(5)",fibonacci(5));
+	
 
 module.exports = fibonacci;
